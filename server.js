7cc89bb288a8036b29309e1cc1beb0197067fdaa
hrishav sharma <211b138@juetguna.in>
2023-11-28 12:33:40 +0530

    SERVER

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname,'.')));
+
+app.listen(8000,()=>console.log("port active at 8000"));
+
